d4f2454f79931a25319f94911002b127580156ca
Keshav-babu <keshavbabu742@gmail.com>
2022-05-17 21:15:43 +0530

    User Location

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { geolocated } from "react-geolocated";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+render() {
+	return this.props.isGeolocationAvailable ? (
+	this.props.isGeolocationEnabled ? (
+		this.props.coords ? (
+		<div>
+			<h1 style={{ color: "green" }}>GeeksForGeeks</h1>
+			<h3 style={{ color: "red" }}>
+			Current latitude and longitude of the user is
+			</h3>
+			<ul>
+			<li>latitude - {this.props.coords.latitude}</li>
+			<li>longitude - {this.props.coords.longitude}</li>
+			</ul>
+		</div>
+		) : (
+		<h1>Getting the location data</h1>
+		)
+	) : (
+		<h1>Please enable location on your browser</h1>
+	)
+	) : (
+	<h1>Please, update your or change the browser </h1>
+	);
 }
-
-export default App;
+}
+export default geolocated()(App);
